e1d76b6df7ae62698510f2e0265228259f62adfc
MariamJan <166922600+MariamJan@users.noreply.github.com>
2024-12-29 15:50:47 +0200

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once(__DIR__ . '/../models/AdminModel.php');
 
@@ -35,42 +34,4 @@ class AdminController
         return $this->admin->deleteUser($userId);
     }
 }
-=======
-<?php
-require_once(__DIR__ . '/../models/AdminModel.php');
-
-class AdminController
-{
-    private $admin;
-
-    public function __construct()
-    {
-        $this->admin = new Admin();
-    }
-
-   
-    public function getUserById(int $userId): ?array
-    {
-        return $this->admin->getUserById($userId);
-    }
-
-    
-    public function addUser(string $username, string $email, string $password, string $usertype, array $courses = []): string
-    {
-        return $this->admin->addUser($username, $email, $password, $usertype, $courses);
-    }
-
-   
-    public function editUser(int $userId, string $username, string $email, ?string $password, string $usertype, array $courses = []): string
-    {
-        return $this->admin->editUser($userId, $username, $email, $password, $usertype, $courses);
-    }
-
-   
-    public function deleteUser(int $userId): string
-    {
-        return $this->admin->deleteUser($userId);
-    }
-}
->>>>>>> 05e937ed01a572bf03bcd649485c169a158eaeff
 ?>
