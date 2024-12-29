9f80cf37dc1a031ff02093011928b01f6a609192
MariamJan <166922600+MariamJan@users.noreply.github.com>
2024-12-29 15:52:48 +0200

    Update AdminDelete.php

--- views/AdminDelete.php
+++ views/AdminDelete.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once('../models/AdminModel.php');
 require_once(__DIR__ . '/../controllers/AdminController.php');
@@ -37,43 +36,3 @@ if (isset($_GET['id'])) {
 
 $connection->close();
 ?>
-=======
-<?php
-require_once('../models/AdminModel.php');
-require_once(__DIR__ . '/../controllers/AdminController.php');
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "e-learning platform";
-
-$connection = new mysqli($servername, $username, $password, $database);
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
-
-
-
-// Instantiate the Admin object
-$admin = new Admin($connection);
-
-// Check if the ID is passed in the URL
-if (isset($_GET['id'])) {
-    $user_id = intval($_GET['id']); // Get the user ID from the URL
-
-    $result = $admin->deleteUser($user_id);
-
-    if ($result === "User deleted successfully!") {
-
-        header("Location: Admin.php?message=User deleted successfully");
-        exit;
-    } else {
-        echo "Error deleting user: " . $result;
-    }
-} else {
-    echo "Invalid user ID";
-}
-
-$connection->close();
-?>
->>>>>>> 05e937ed01a572bf03bcd649485c169a158eaeff
